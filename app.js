8ba7f6fbad63cec3cc25e5741e12745843e96866
Sherif H <sherifh@sfu.ca>
2021-02-21 11:45:45 -0800

    first perm

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+const express = require('express');
+const app = express();
+const path = require('path');
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
-		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
-		this.camera.position.set( 0, 0, 4 );
-        
-		this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xaaaaaa );
+app.use(express.static(__dirname + '/public'));
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
-		this.scene.add(ambient);
-        
-        const light = new THREE.DirectionalLight();
-        light.position.set( 0.2, 1, 1);
-        this.scene.add(light);
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		container.appendChild( this.renderer.domElement );
-		
-        const geometry = new THREE.BoxBufferGeometry();
-        const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
-
-        this.mesh = new THREE.Mesh( geometry, material );
-        
-        this.scene.add(this.mesh);
-        
-        const controls = new OrbitControls( this.camera, this.renderer.domElement );
-        
-        this.renderer.setAnimationLoop(this.render.bind(this));
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight );  
-    }
-    
-	render( ) {   
-        this.mesh.rotateY( 0.01 );
-        this.renderer.render( this.scene, this.camera );
-    }
-}
-
-export { App };
+app.listen(3000, () => {
+    console.log('Visit http://127.0.0.1:3000')
+});
